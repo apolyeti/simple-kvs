c8079c4ef9abe2afc7e5525c74d15485f64d9329
apolyeti <azhandarveen@gmail.com>
2024-08-03 17:12:47 -0700

    feat: grpc kvs server working

--- main.go
+++ main.go
@@ -1,29 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 
 	"github.com/apolyeti/simple-kvs/kvs"
+	pb "github.com/apolyeti/simple-kvs/proto"
 )
 
-func main() {
-	store := kvs.New()
-	fmt.Println("Setting key1 to 1")
-	store.Set("key1", 1)
-	fmt.Println("Setting key2 to 2")
-	store.Set("key2", 2)
-	fmt.Println("Getting key1")
-	value, err := store.Get("key1")
+type server struct {
+	pb.UnimplementedKvsServer
+	store *kvs.Store
+}
+
+func (s *server) Set(ctx context.Context, in *pb.SetRequest) (*pb.SetResponse, error) {
+	s.store.Set(in.Key, int(in.Value))
+	return &pb.SetResponse{}, nil
+}
+
+func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
+	value, err := s.store.Get(in.Key)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(value)
+		return nil, err
 	}
-	fmt.Println("Getting key2")
-	value, err = store.Get("key2")
+	return &pb.GetResponse{Value: int32(value)}, nil
+}
+
+func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	s.store.Delete(in.Key)
+	return &pb.DeleteResponse{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(value)
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	store := kvs.New()
+	pb.RegisterKvsServer(s, &server{store: store})
+	log.Printf("Starting server on %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
